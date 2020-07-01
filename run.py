5466f1256574077af3adc63a9fad74aa86e328d6
The-element <nicke.jonsson@live.com>
2020-07-01 18:41:17 +0000

    more logic added

--- run.py
+++ run.py
@@ -34,6 +34,19 @@ def about():
     return render_template("about.html", page_title="About", company=data)  # Then we pass that data into the return, called company.
 
 
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    
+    return render_template("member.html", member=member)
+
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="Contact")
